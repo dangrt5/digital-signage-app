ca0e45a3e92a4ffd7a58bdd376f9c2c90e862203
dangrt5 <randy.dang05@gmail.com>
2019-01-08 13:19:05 -0800

    added header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,20 @@
 import React from "react";
+import "./Header.css";
+import backButton from "../assets/images/back.png";
+import nextButton from "../assets/images/next.png";
 
 class Header extends React.Component {
   render() {
+    const { title } = this.props;
     return (
       <div className="header">
-        <div className="back-button" />
-        <div className="title" />
-        <div className="forward-button" />
+        <div className="back-button">
+          <img src={backButton} alt="Previous Page" />
+        </div>
+        <div className="title">{title}</div>
+        <div className="forward-button">
+          <img src={nextButton} alt="Next Page" />
+        </div>
       </div>
     );
   }
